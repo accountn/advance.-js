575d06c6f3d42aba91c65b8c9af9ded6827aa647
helal <helalimage12345@gmail.com>
2021-05-29 22:25:40 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+let age;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1,num2)
+}
+
+const premik = {name: 'bindas', phone: 0247593};
+console.log(premik.gf);
+
+const fun = undefined;
+console.log(fun);
+
+let ages = [2,4,5,6];
+console.log(ages[9]);
